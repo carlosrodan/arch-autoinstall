3965992d391ecbf3db0e151da5a4a2a2fd9f72d4
Your Name <you@example.com>
2025-11-11 17:26:41 +0100

    Added zsh,oh-my-zsh and powerlevel10k + plugings

--- post-install.sh
+++ post-install.sh
@@ -193,3 +193,52 @@ sudo systemctl enable --now orphan-clean.timer || true
 
 echog "Post-install tasks complete. You should be auto-logged in on tty1 and Hyperland will start."
 echog "If you need to edit Hypr config, switch to another TTY (Ctrl+Alt+F2) and edit ~/.config/hypr/hyprland.conf"
+
+##############################
+# Install Zsh, Oh My Zsh, Powerlevel10k
+##############################
+
+echog "Installing Zsh..."
+sudo pacman -S --noconfirm zsh
+
+echog "Setting Zsh as default shell for user $USERNAME..."
+chsh -s "$(which zsh)" "$USERNAME" || echow "Failed to change default shell. You may need to log out and back in."
+
+# Install Oh My Zsh if not already installed
+if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
+  echog "Installing Oh My Zsh..."
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended || echow "Oh My Zsh installation failed."
+else
+  echog "Oh My Zsh already installed."
+fi
+
+# Install Powerlevel10k theme
+if [[ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]]; then
+  echog "Installing Powerlevel10k theme..."
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+    "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+else
+  echog "Powerlevel10k already installed."
+fi
+
+# Configure .zshrc to use Powerlevel10k
+ZSHRC_FILE="$HOME/.zshrc"
+if ! grep -q "ZSH_THEME=\"powerlevel10k/powerlevel10k\"" "$ZSHRC_FILE" 2>/dev/null; then
+  echog "Configuring .zshrc to use Powerlevel10k..."
+  sed -i 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/' "$ZSHRC_FILE" || \
+    echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> "$ZSHRC_FILE"
+fi
+
+# Enable recommended plugins
+if ! grep -q '^plugins=' "$ZSHRC_FILE"; then
+  echog "Adding recommended plugins to .zshrc..."
+  echo 'plugins=(git zsh-autosuggestions zsh-syntax-highlighting)' >> "$ZSHRC_FILE"
+else
+  echog "Updating plugins in .zshrc..."
+  sed -i 's/^plugins=.*/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' "$ZSHRC_FILE"
+fi
+
+echog "Zsh + Oh My Zsh + Powerlevel10k installation complete."
+echog "Plugins enabled: git, zsh-autosuggestions, zsh-syntax-highlighting"
+echog "You may need to restart your terminal or log out/in to use Zsh as default shell."
+
